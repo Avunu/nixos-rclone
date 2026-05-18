13d9c5c274427a22171f29b5e52d572379952157
Kevin Shenk <kevin@avu.nu>
2026-05-17 20:12:42 -0400

    chore: cleanup files

--- filters/docx2md.nix
+++
@@ -1,8 +0,0 @@
-{ haskellPackages, runCommand }:
-let
-  haskellEnv = haskellPackages.ghcWithPackages (ps: [ ps.pandoc ]);
-in
-runCommand "docx2md-filter" { nativeBuildInputs = [ haskellEnv ]; } ''
-  mkdir -p $out/bin
-  ghc -outputdir "$TMPDIR" ${./docx2md.hs} -o $out/bin/docx2md
-''
--- filters/md2docx.nix
+++
@@ -1,8 +0,0 @@
-{ haskellPackages, runCommand }:
-let
-  haskellEnv = haskellPackages.ghcWithPackages (ps: [ ps.pandoc ]);
-in
-runCommand "md2docx-filter" { nativeBuildInputs = [ haskellEnv ]; } ''
-  mkdir -p $out/bin
-  ghc -outputdir "$TMPDIR" ${./md2docx.hs} -o $out/bin/md2docx
-''
